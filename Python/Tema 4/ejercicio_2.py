e0be3a3f929596cedde6f28425ae503b481a6291
Fabián Pavia <68711515+fabian4613@users.noreply.github.com>
2022-05-29 03:08:29 -0300

    Update ejercicio_2.py

--- Python/Tema 4/ejercicio_2.py
+++ Python/Tema 4/ejercicio_2.py
@@ -1,11 +1,13 @@
 # Usaremos la función input("mensaje") para solicitar algo al usuario por consola
-numero_inicial: int = int(input('Introduce un número: '))
-numero_final: int = int(input('Inroduce otro número: '))
-numeros_impares: [int] = []
+
+numero_inicial: int = int(input('Introduce un numero: '))
+numero_final: int = int(input('Introduce otro numero: '))
+numeros_impares: (int) = []
 
 # Mientras el número final sea menor al número inicial pedira que el segundo número sea mayor al primero.
+
 while numero_final <= numero_inicial:
-    numero_final: int = int(input('El segundo número debe ser mayor que el primero. Inroduce otro número: '))
+        numero_final: int = int(input('El segundo número debe ser mayor que el primero. Introduce otro número: ' ))
 
 # Recorremos el rango de los números para mostrar los números impares
 for i in range(numero_inicial, numero_final+1):
